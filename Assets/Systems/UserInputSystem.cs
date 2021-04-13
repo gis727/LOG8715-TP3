9ebeb54bfcee7a46342272c33b2a78d5a0c3dd2e
Ismael Gbian <ismael.gbian@gmail.com>
2021-04-13 17:11:29 -0400

    refactor: Cleaned up UserInput system

--- Assets/Systems/UserInputSystem.cs
+++ Assets/Systems/UserInputSystem.cs
@@ -16,24 +16,18 @@ public class UserInputSystem : ISystem
     {
         if (ECSManager.Instance.RunningFastForward) return;
         
-        if (ECSManager.Instance.NetworkManager.isClient)
-        {
-            DoClientStuff();
-        }
-        else if (ECSManager.Instance.NetworkManager.isServer)
-        {
-            DoServerStuff();
-        }
+        if      (ECSManager.Instance.NetworkManager.isClient) UpdateClientSystem();
+        else if (ECSManager.Instance.NetworkManager.isServer) UpdateServerSystem();
     }
 
-    void DoClientStuff()
+    void UpdateClientSystem()
     {
-        bool inputDetected = false;
-        int speed = 4;
         uint clientId = (uint)ECSManager.Instance.NetworkManager.LocalClientId;
         int currentTime = Utils.SystemTime;
+
         if (ComponentsManager.Instance.TryGetComponent(clientId, out ShapeComponent shapeComponent))
         {
+            // Build a new message
             ReplicationMessage msg = new ReplicationMessage();
             msg.entityId = clientId;
             msg.timeCreated = currentTime;
@@ -45,35 +39,14 @@ public class UserInputSystem : ISystem
             msg.inputW = 0;
             msg.inputS = 0;
             msg.inputD = 0;
-            shapeComponent.speed = Vector2.zero;
-            
-            if (Input.GetKey(KeyCode.A))
-            {
-                shapeComponent.speed = Vector2.left * speed;
-                msg.inputA = 1;
-                inputDetected = true;
-            }
-            else if (Input.GetKey(KeyCode.W))
-            {
-                shapeComponent.speed = Vector2.up * speed;
-                msg.inputW = 1;
-                inputDetected = true;
-            }
-            else if (Input.GetKey(KeyCode.S))
-            {
-                shapeComponent.speed = Vector2.down * speed;
-                msg.inputS = 1;
-                inputDetected = true;
-            }
-            else if (Input.GetKey(KeyCode.D))
-            {
-                shapeComponent.speed = Vector2.right * speed;
-                msg.inputD = 1;
-                inputDetected = true;
-            }
+
+            // Get user inputs
+            bool inputDetected = GetUserInput(ref msg, ref shapeComponent, true);
+
+            // Save the inputs if required
             if (ECSManager.Instance.Config.enableInputPrediction) ComponentsManager.Instance.SetComponent<ShapeComponent>(clientId, shapeComponent);
 
-            // save the world state in history
+            
             ComponentsManager.Instance.ForEach<ShapeComponent, UserInputComponent>((entityID, entityShape, entityUserInput) =>
             {
                 ReplicationMessage entityMsg = new ReplicationMessage();
@@ -92,63 +65,48 @@ public class UserInputSystem : ISystem
                         entityMsg.inputS = msg.inputS;
                         entityMsg.inputD = msg.inputD;
                     }
+
+                    // Put the new input message in the queue to server
                     entityUserInput.pendingInputsMessages.Add(msg);
                 }
 
+                // save in history
                 entityUserInput.inputHistory.Add(entityMsg);
 
                 ComponentsManager.Instance.SetComponent<UserInputComponent>(entityID, entityUserInput);
             });
             
-            // clear old messages
-            ComponentsManager.Instance.ForEach<UserInputComponent>((entityID,entityUserInput) =>
-            {
-                entityUserInput.inputHistory.RemoveAll(x => (currentTime - x.timeCreated) > 2000);
-                ComponentsManager.Instance.SetComponent<UserInputComponent>(entityID, entityUserInput);
-            });
+            // Clear old messages
+            ClearOldInputs(currentTime);
         }
     }
-    void DoServerStuff()
+
+    void UpdateServerSystem()
     {
-        int speed = 4;
         ComponentsManager.Instance.ForEach<UserInputComponent>((entityID, userInput) =>
         {
             bool shapeSpawned = ComponentsManager.Instance.TryGetComponent(entityID, out ShapeComponent shapeComp);
             bool isPlayer = ComponentsManager.Instance.EntityContains<PlayerComponent>(entityID);
             if (shapeSpawned && isPlayer)
             {
-                for (int i=0; i < userInput.pendingInputsMessages.Count; i++)
+                for (int i = 0; i < userInput.pendingInputsMessages.Count; i++)
                 {   
                     ShapeComponent shapeComponent = ComponentsManager.Instance.GetComponent<ShapeComponent>(entityID);
-                    shapeComponent.speed = Vector2.zero;
-                    var msg = userInput.pendingInputsMessages[i];
-                    if (msg.inputA == 1)
-                    {
-                        shapeComponent.speed = Vector2.left * speed;
-                    }
-                    else if (msg.inputW == 1)
-                    {
-                        shapeComponent.speed = Vector2.up * speed;
-                    }
-                    else if (msg.inputS == 1)
-                    {
-                        shapeComponent.speed = Vector2.down * speed;
-                    }
-                    else if (msg.inputD == 1)
-                    {
-                        shapeComponent.speed = Vector2.right * speed;
-                    }
+                    
+                    // Get the user input coming from the client
+                    ReplicationMessage msg = userInput.pendingInputsMessages[i];
+                    GetUserInput(ref msg, ref shapeComponent, false);
+
                     ComponentsManager.Instance.SetComponent<ShapeComponent>(entityID, shapeComponent);
 
-                    // prepare fast forward
-                    // set a replication message to let the world know that this entity is concerned by fast forward
+                    // Prepare fast forward:
+                    // Set a replication message to let the world know that this entity is concerned by fast forward
                     userInput.fastForwardInputsMessages = new List<ReplicationMessage>{new ReplicationMessage()};
                     ComponentsManager.Instance.SetComponent<UserInputComponent>(entityID, userInput);
                     
-                    // fast forward (simulate input)
                     ECSManager.Instance.FastForward(1);
 
-                    // reset 0 speed
+                    // Reset speed to 0
                     ShapeComponent newShapeComponent = ComponentsManager.Instance.GetComponent<ShapeComponent>(entityID);
                     newShapeComponent.speed = Vector2.zero;
                     ComponentsManager.Instance.SetComponent<ShapeComponent>(entityID, newShapeComponent);
@@ -156,6 +114,51 @@ public class UserInputSystem : ISystem
                 userInput.pendingInputsMessages.Clear();
                 ComponentsManager.Instance.SetComponent<UserInputComponent>(entityID, userInput);
             }
+        });
+    }
+
+    // if 'readFromInput == true': Reads the inputs from user keyboard and populates 'msg' and 'shapeComponent' accordingly
+    // else: Reads the inputs inside 'msg' and populates 'shapeComponent' accordingly
+    private bool GetUserInput(ref ReplicationMessage msg, ref ShapeComponent shapeComponent, bool readFromInput)
+    {
+        int speed = 4;
+        bool inputDetected = false;
+        shapeComponent.speed = Vector2.zero;
+        if ((!readFromInput && msg.inputA == 1) || (readFromInput && Input.GetKey(KeyCode.A)))
+        {
+            if (readFromInput) msg.inputA = 1;
+            shapeComponent.speed = Vector2.left * speed;
+            inputDetected = true;
+        }
+        else if ((!readFromInput && msg.inputW == 1) || (readFromInput && Input.GetKey(KeyCode.W)))
+        {
+            if (readFromInput) msg.inputW = 1;
+            shapeComponent.speed = Vector2.up * speed;
+            inputDetected = true;
+        }
+        else if ((!readFromInput && msg.inputS == 1) || (readFromInput && Input.GetKey(KeyCode.S)))
+        {
+            if (readFromInput) msg.inputS = 1;
+            shapeComponent.speed = Vector2.down * speed;
+            inputDetected = true;
+        }
+        else if ((!readFromInput && msg.inputD == 1) || (readFromInput && Input.GetKey(KeyCode.D)))
+        {
+            if (readFromInput) msg.inputD = 1;
+            shapeComponent.speed = Vector2.right * speed;
+            inputDetected = true;
+        }
+        return inputDetected;
+    }
+
+    // Clears inputs older than 'maxAge' from the input history
+    private void ClearOldInputs(int currentTime)
+    {
+        int maxAge = 2000;
+        ComponentsManager.Instance.ForEach<UserInputComponent>((entityID,entityUserInput) =>
+        {
+            entityUserInput.inputHistory.RemoveAll(x => (currentTime - x.timeCreated) > maxAge);
+            ComponentsManager.Instance.SetComponent<UserInputComponent>(entityID, entityUserInput);
         });
     }
 }
